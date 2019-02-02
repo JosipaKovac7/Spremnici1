2167cf24b4959eaa4cf29d129ca83b4ca9fcb056
JosipaKovac7 <38297147+JosipaKovac7@users.noreply.github.com>
2019-02-02 11:39:37 +0100

    Add files via upload

---
+++ Spremnici/Spremnik.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spremnici
+{
+    class Spremnik
+    {
+        public string naziv;
+        protected decimal kapacitet;
+        protected decimal popunjenost;
+
+        public Spremnik()
+        {
+            kapacitet = 3;
+            popunjenost = 0;
+        }
+
+        public void postaviKapacitet(decimal _kapacitet)
+        {
+            kapacitet = _kapacitet;
+        }
+
+        public decimal vratiPopunjenost()
+        {
+            return popunjenost;
+        }
+
+        public void postaviNaziv(string _naziv)
+        {
+            naziv = _naziv;
+        }
+
+        public string vratiNaziv()
+        {
+            return naziv;
+        }
+        
+    }
+}
